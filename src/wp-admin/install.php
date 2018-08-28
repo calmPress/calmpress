0d79dcbe9547d4cb170eee68de120f03c23ede5f
mark kaplun <mark@marksw.com>
2018-08-28 10:06:48 +0300

    more wp to cp replacement #12

--- src/wp-admin/install.php
+++ src/wp-admin/install.php
@@ -16,9 +16,9 @@ if ( false ) {
 	<title>Error: PHP is not running</title>
 </head>
 <body class="wp-core-ui">
-	<p id="logo"><a href="https://wordpress.org/">WordPress</a></p>
+	<p id="logo"><a href="https://wordpress.org/">calmPress</a></p>
 	<h1>Error: PHP is not running</h1>
-	<p>WordPress requires that your web server is running PHP. Your server does not have PHP installed, or PHP is turned off.</p>
+	<p>calmPress requires that your web server is running PHP. Your server does not have PHP installed, or PHP is turned off.</p>
 </body>
 </html>
 <?php
