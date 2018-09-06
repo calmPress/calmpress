385689250d1488b94c36b9a9962d24575214c5b7
mark kaplun <mark@marksw.com>
2018-09-06 11:29:55 +0300

    return the calmPress version where WP used to return its version in version related API #19

--- src/wp-includes/general-template.php
+++ src/wp-includes/general-template.php
@@ -693,8 +693,7 @@ function get_bloginfo( $show = '', $filter = 'raw' ) {
 			$output = get_option('html_type');
 			break;
 		case 'version':
-			global $wp_version;
-			$output = $wp_version;
+			$output = calmpress_version();
 			break;
 		case 'language':
 			/* translators: Translate this to the correct language tag for your locale,
