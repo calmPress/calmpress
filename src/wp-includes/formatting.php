7537f8cce64754937e50b4dca8b06467a94c93e9
mark kaplun <mark@marksw.com>
2018-10-10 04:34:41 +0300

    remove capitalP_dangit #6

--- src/wp-includes/formatting.php
+++ src/wp-includes/formatting.php
@@ -4776,34 +4776,6 @@ function wp_basename( $path, $suffix = '' ) {
 }
 
 /**
- * Forever eliminate "Wordpress" from the planet (or at least the little bit we can influence).
- *
- * Violating our coding standards for a good function name.
- *
- * @since 3.0.0
- *
- * @staticvar string|false $dblq
- *
- * @param string $text The text to be modified.
- * @return string The modified text.
- */
-function capital_P_dangit( $text ) {
-	// Simple replacement for titles
-	$current_filter = current_filter();
-	if ( 'the_title' === $current_filter || 'wp_title' === $current_filter )
-		return str_replace( 'Wordpress', 'WordPress', $text );
-	// Still here? Use the more judicious replacement
-	static $dblq = false;
-	if ( false === $dblq ) {
-		$dblq = _x( '&#8220;', 'opening curly double quote' );
-	}
-	return str_replace(
-		array( ' Wordpress', '&#8216;Wordpress', $dblq . 'Wordpress', '>Wordpress', '(Wordpress' ),
-		array( ' WordPress', '&#8216;WordPress', $dblq . 'WordPress', '>WordPress', '(WordPress' ),
-	$text );
-}
-
-/**
  * Sanitize a mime type
  *
  * @since 3.1.3
