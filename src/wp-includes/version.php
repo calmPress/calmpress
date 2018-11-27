2592ca6883c354f5418f4b53baffdb37b01c7f48
mark kaplun <mark@marksw.com>
2018-11-27 17:19:25 +0200

    release 0.9.9 ready!

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -39,7 +39,7 @@ if ( ! function_exists( 'calmpress_version' ) ) {
 	 * @return string The version string.
 	 */
 	function calmpress_version() {
-		return '0.9.9-beta4';
+		return '0.9.9';
 	}
 }
 
