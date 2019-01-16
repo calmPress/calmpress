28c151a4fe6da9ebabf4f926dcf9f3eaa057baf6
mark kaplun <mark@marksw.com>
2019-01-16 18:40:37 +0200

    Fix PHP error regard uninitialized variable

--- src/wp-admin/includes/upgrade.php
+++ src/wp-admin/includes/upgrade.php
@@ -196,7 +196,7 @@ Commenter avatars come from <a href="https://gravatar.com">Gravatar</a>.' );
 
 	if ( empty( $first_page ) ) {
 		/* translators: first page content */
-		$first_page .= __( "This is an example page. It's different from a blog post because it will stay in one place and will show up in your site navigation (in most themes). Most people start with an About page that introduces them to potential site visitors. It might say something like this:" );
+		$first_page = __( "This is an example page. It's different from a blog post because it will stay in one place and will show up in your site navigation (in most themes). Most people start with an About page that introduces them to potential site visitors. It might say something like this:" );
 		$first_page .= "\n\n";
 
 		$first_page .= "<blockquote>";
