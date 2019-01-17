8c9aea7508ab690f4fa011130334f430eeb7be6e
mark kaplun <mark@marksw.com>
2019-01-17 20:23:51 +0200

    Emoji detection is removed, remove the relevant tests

--- tests/phpunit/tests/formatting/Emoji.php
+++ tests/phpunit/tests/formatting/Emoji.php
@@ -9,56 +9,12 @@ class Tests_Formatting_Emoji extends WP_UnitTestCase {
 	private $png_cdn = 'https://s.w.org/images/core/emoji/11/72x72/';
 	private $svn_cdn = 'https://s.w.org/images/core/emoji/11/svg/';
 
-	/**
-	 * @ticket 36525
-	 */
-	public function test_unfiltered_emoji_cdns() {
-		$output = get_echo( '_print_emoji_detection_script' );
-
-		$this->assertContains( wp_json_encode( $this->png_cdn ), $output );
-		$this->assertContains( wp_json_encode( $this->svn_cdn ), $output );
-	}
-
 	public function _filtered_emoji_svn_cdn( $cdn = '' ) {
 		return 'https://s.wordpress.org/images/core/emoji/svg/';
 	}
 
-	/**
-	 * @ticket 36525
-	 */
-	public function test_filtered_emoji_svn_cdn() {
-		$filtered_svn_cdn = $this->_filtered_emoji_svn_cdn();
-
-		add_filter( 'emoji_svg_url', array( $this, '_filtered_emoji_svn_cdn' ) );
-
-		$output = get_echo( '_print_emoji_detection_script' );
-
-		$this->assertContains( wp_json_encode( $this->png_cdn ), $output );
-		$this->assertNotContains( wp_json_encode( $this->svn_cdn ), $output );
-		$this->assertContains( wp_json_encode( $filtered_svn_cdn ), $output );
-
-		remove_filter( 'emoji_svg_url', array( $this, '_filtered_emoji_svn_cdn' ) );
-	}
-
 	public function _filtered_emoji_png_cdn( $cdn = '' ) {
 		return 'https://s.wordpress.org/images/core/emoji/png_cdn/';
-	}
-
-	/**
-	 * @ticket 36525
-	 */
-	public function test_filtered_emoji_png_cdn() {
-		$filtered_png_cdn = $this->_filtered_emoji_png_cdn();
-
-		add_filter( 'emoji_url', array( $this, '_filtered_emoji_png_cdn' ) );
-
-		$output = get_echo( '_print_emoji_detection_script' );
-
-		$this->assertContains( wp_json_encode( $filtered_png_cdn ), $output );
-		$this->assertNotContains( wp_json_encode( $this->png_cdn ), $output );
-		$this->assertContains( wp_json_encode( $this->svn_cdn ), $output );
-
-		remove_filter( 'emoji_url', array( $this, '_filtered_emoji_png_cdn' ) );
 	}
 
 	/**
