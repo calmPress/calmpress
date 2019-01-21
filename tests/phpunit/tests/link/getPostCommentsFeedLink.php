f8d586465afc61317b6580ac3fb09050f5a30945
mark kaplun <mark@marksw.com>
2019-01-21 08:11:57 +0200

    Adjust test to new URL default structure

--- tests/phpunit/tests/link/getPostCommentsFeedLink.php
+++ tests/phpunit/tests/link/getPostCommentsFeedLink.php
@@ -8,10 +8,7 @@ class Tests_Link_GetPostCommentsFeedLink extends WP_UnitTestCase {
 		$post_id = self::factory()->post->create();
 
 		$link = get_post_comments_feed_link( $post_id );
-		$expected = add_query_arg( array(
-			'feed' => get_default_feed(),
-			'p' => $post_id
-		), home_url( '/' ) );
+		$expected = get_permalink( $post_id ) . 'feed/' ;
 
 		$this->assertEquals( $expected, $link );
 	}
@@ -35,10 +32,7 @@ class Tests_Link_GetPostCommentsFeedLink extends WP_UnitTestCase {
 		) );
 
 		$link = get_post_comments_feed_link( $attachment_id );
-		$expected = add_query_arg( array(
-			'feed' => get_default_feed(),
-			'p' => $attachment_id
-		), home_url( '/' ) );
+		$expected = get_permalink( $post_id ) . 'attachment/' . $attachment_id . '/feed/' ;
 
 		$this->assertEquals( $expected, $link );
 	}
@@ -78,32 +72,4 @@ class Tests_Link_GetPostCommentsFeedLink extends WP_UnitTestCase {
 		$this->assertEquals( $expected, $link );
 	}
 
-	public function test_unattached_link() {
-		$attachment_id = self::factory()->attachment->create_object( 'image.jpg', 0, array(
-			'post_mime_type' => 'image/jpeg',
-			'post_type' => 'attachment'
-		) );
-
-		$link = get_post_comments_feed_link( $attachment_id );
-		$expected = add_query_arg( array(
-			'feed' => get_default_feed(),
-			'attachment_id' => $attachment_id
-		), home_url( '/' ) );
-
-		$this->assertEquals( $expected, $link );
-	}
-
-	public function test_unattached_pretty_link() {
-		$this->set_permalink_structure( '/%year%/%monthnum%/%day%/%postname%/' );
-
-		$attachment_id = self::factory()->attachment->create_object( 'image.jpg', 0, array(
-			'post_mime_type' => 'image/jpeg',
-			'post_type' => 'attachment'
-		) );
-
-		$link = get_post_comments_feed_link( $attachment_id );
-		$expected = add_query_arg( 'attachment_id', $attachment_id, home_url( '/feed/' ) );
-
-		$this->assertEquals( $expected, $link );
-	}
 }
