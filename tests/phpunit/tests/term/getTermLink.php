a05aaa861e1d4e5052632c79c676f302796280dd
mark kaplun <mark@marksw.com>
2019-01-22 09:44:15 +0200

    Adjust for url structure

--- tests/phpunit/tests/term/getTermLink.php
+++ tests/phpunit/tests/term/getTermLink.php
@@ -24,7 +24,7 @@ class Tests_Term_GetTermLink extends WP_UnitTestCase {
 		$term = intval( $t1 );
 
 		$actual = get_term_link( $term, 'wptests_tax' );
-		$this->assertContains( 'wptests_tax=foo', $actual );
+		$this->assertContains( 'wptests_tax/foo', $actual );
 	}
 
 	public function test_numeric_string_should_be_interpreted_as_term_slug() {
@@ -40,7 +40,7 @@ class Tests_Term_GetTermLink extends WP_UnitTestCase {
 		$term = (string) $t1;
 
 		$actual = get_term_link( $term, 'wptests_tax' );
-		$this->assertContains( 'wptests_tax=' . $term, $actual );
+		$this->assertContains( 'wptests_tax/' . $term, $actual );
 	}
 
 	public function test_invalid_term_should_return_wp_error() {
@@ -49,24 +49,10 @@ class Tests_Term_GetTermLink extends WP_UnitTestCase {
 	}
 
 	public function test_category_should_use_cat_query_var_with_term_id() {
-		$c = self::factory()->category->create();
+		$c = self::factory()->category->create( ['name' => 'tets']);
 
 		$actual = get_term_link( $c, 'category' );
-		$this->assertContains( 'cat=' . $c, $actual );
-	}
-
-	public function test_taxonomy_with_query_var_should_use_that_query_var_with_term_slug() {
-		register_taxonomy( 'wptests_tax2', 'post', array(
-			'query_var' => 'foo',
-		) );
-
-		$t = self::factory()->term->create( array(
-			'taxonomy' => 'wptests_tax2',
-			'slug' => 'bar',
-		) );
-
-		$actual = get_term_link( $t, 'wptests_tax2' );
-		$this->assertContains( 'foo=bar', $actual );
+		$this->assertContains( '/category/tets/', $actual );
 	}
 
 	public function test_taxonomy_without_query_var_should_use_taxonomy_query_var_and_term_query_var_with_term_slug() {
@@ -80,8 +66,8 @@ class Tests_Term_GetTermLink extends WP_UnitTestCase {
 		) );
 
 		$actual = get_term_link( $t, 'wptests_tax2' );
-		$this->assertContains( 'taxonomy=wptests_tax2', $actual );
-		$this->assertContains( 'term=bar', $actual );
+		$this->assertContains( '/wptests_tax2', $actual );
+		$this->assertContains( '/bar', $actual );
 	}
 
 	public function test_taxonomy_permastruct_with_hierarchical_rewrite_should_put_term_ancestors_in_link() {
