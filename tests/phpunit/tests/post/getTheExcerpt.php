2c2c5aec56df590157eeff0ea53990912416fcd0
mark kaplun <mark@marksw.com>
2019-01-22 09:25:22 +0200

    No password protction round here

--- tests/phpunit/tests/post/getTheExcerpt.php
+++ tests/phpunit/tests/post/getTheExcerpt.php
@@ -34,18 +34,6 @@ class Tests_Post_GetTheExcerpt extends WP_UnitTestCase {
 
 	/**
 	 * @ticket 27246
-	 * @ticket 35486
-	 */
-	public function test_the_excerpt_password_protected_post() {
-		$post = self::factory()->post->create_and_get( array( 'post_excerpt' => 'Post excerpt', 'post_password' => '1234' ) );
-		$this->assertSame( 'There is no excerpt because this is a protected post.', get_the_excerpt( $post ) );
-
-		$GLOBALS['post'] = $post;
-		$this->assertSame( "<p>There is no excerpt because this is a protected post.</p>\n", get_echo( 'the_excerpt' ) );
-	}
-
-	/**
-	 * @ticket 27246
 	 */
 	public function test_the_excerpt_specific_post() {
 		$GLOBALS['post'] = self::factory()->post->create_and_get( array( 'post_excerpt' => 'Foo' ) );
