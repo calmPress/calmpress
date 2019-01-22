f29edd6378f971ec6170958302f0c4c1b37b33e2
mark kaplun <mark@marksw.com>
2019-01-22 17:21:06 +0200

    Adjust for author taxonomy

--- tests/phpunit/tests/rest-api/rest-post-types-controller.php
+++ tests/phpunit/tests/rest-api/rest-post-types-controller.php
@@ -59,7 +59,7 @@ class WP_Test_REST_Post_Types_Controller extends WP_Test_REST_Controller_Testcas
 		$response = $this->server->dispatch( $request );
 		$this->check_post_type_object_response( 'view', $response );
 		$data = $response->get_data();
-		$this->assertEquals( array( 'category', 'post_tag' ), $data['taxonomies'] );
+		$this->assertEquals( array( 'category', 'post_tag', 'calm_authors' ), $data['taxonomies'] );
 	}
 
 	public function test_get_item_page() {
@@ -67,7 +67,7 @@ class WP_Test_REST_Post_Types_Controller extends WP_Test_REST_Controller_Testcas
 		$response = $this->server->dispatch( $request );
 		$this->check_post_type_object_response( 'view', $response, 'page' );
 		$data = $response->get_data();
-		$this->assertEquals( array(), $data['taxonomies'] );
+		$this->assertEquals( array( 'calm_authors' ), $data['taxonomies'] );
 	}
 
 	public function test_get_item_invalid_type() {
