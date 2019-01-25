930f5b23e386c0af48d204c0d297f0f0d2db778d
mark kaplun <mark@marksw.com>
2019-01-25 17:45:13 +0200

    no support for the bulk meta change on post edit

--- tests/phpunit/tests/ajax/AddMeta.php
+++ tests/phpunit/tests/ajax/AddMeta.php
@@ -36,36 +36,4 @@ class Tests_Ajax_AddMeta extends WP_Ajax_UnitTestCase {
 
 		$this->assertEquals( '', get_post_meta( $p, 'testkey', true ) );
 	}
-
-	/**
-	 * @ticket 43559
-	 */
-	public function test_post_update_meta_empty_is_allowed_ajax() {
-		$p = self::factory()->post->create();
-
-		$m = add_post_meta( $p, 'testkey', 'hello' );
-
-		// Become an administrator.
-		$this->_setRole( 'administrator' );
-
-		$_POST = array(
-			'_ajax_nonce-add-meta' => wp_create_nonce( 'add-meta' ),
-			'post_id' => $p,
-			'meta' => array(
-				$m => array(
-					'key'   => 'testkey',
-					'value' => '',
-				),
-			),
-		);
-
-		// Make the request.
-		try {
-			$this->_handleAjax( 'add-meta' );
-		} catch ( WPAjaxDieContinueException $e ) {
-			unset( $e );
-		}
-
-		$this->assertEquals( '', get_post_meta( $p, 'testkey', true ) );
-	}
 }
