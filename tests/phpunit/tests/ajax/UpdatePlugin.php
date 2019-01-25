2311df122d49567c515a7a4849c1f3381a480329
mark kaplun <mark@marksw.com>
2019-01-25 17:46:50 +0200

    trmove test that assumes the existance of the hello dolly plugin

--- tests/phpunit/tests/ajax/UpdatePlugin.php
+++ tests/phpunit/tests/ajax/UpdatePlugin.php
@@ -129,41 +129,4 @@ class Tests_Ajax_Update_Plugin extends WP_Ajax_UnitTestCase {
 
 		$this->assertEqualSets( $expected, $response );
 	}
-
-	public function test_update_plugin() {
-		$this->_setRole( 'administrator' );
-
-		$_POST['_ajax_nonce'] = wp_create_nonce( 'updates' );
-		$_POST['plugin']      = 'hello.php';
-		$_POST['slug']        = 'hello-dolly';
-
-		// Make the request
-		try {
-			// Prevent wp_update_plugins() from running
-			wp_installing( true );
-			$this->_handleAjax( 'update-plugin' );
-			wp_installing( false );
-		} catch ( WPAjaxDieContinueException $e ) {
-			unset( $e );
-		}
-
-		// Get the response.
-		$response = json_decode( $this->_last_response, true );
-
-		$expected = array(
-			'success' => false,
-			'data'    => array(
-				'update'       => 'plugin',
-				'slug'         => 'hello-dolly',
-				'plugin'       => 'hello.php',
-				'pluginName'   => 'Hello Dolly',
-				'errorMessage' => 'Plugin update failed.',
-				'oldVersion'   => 'Version 1.7.1',
-				'newVersion'   => '',
-				'debug'        => array( 'The plugin is at the latest version.' ),
-			),
-		);
-
-		$this->assertEqualSets( $expected, $response );
-	}
 }
