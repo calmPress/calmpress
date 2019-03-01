cad6ee9e6dedfd55ae3afa0182da0f030cd96656
mark kaplun <mark@marksw.com>
2019-03-01 20:54:11 +0200

    Remove duplicated function after merge

--- src/wp-includes/load.php
+++ src/wp-includes/load.php
@@ -1268,24 +1268,3 @@ function wp_is_json_request() {
 	return false;
 
 }
-
-/**
- * Checks whether current request is a JSON request, or is expecting a JSON response.
- *
- * @since 5.0.0
- *
- * @return bool True if Accepts or Content-Type headers contain application/json, false otherwise.
- */
-function wp_is_json_request() {
-
-	if ( isset( $_SERVER['HTTP_ACCEPT'] ) && false !== strpos( $_SERVER['HTTP_ACCEPT'], 'application/json' ) ) {
-		return true;
-	}
-
-	if ( isset( $_SERVER['CONTENT_TYPE'] ) && 'application/json' === $_SERVER['CONTENT_TYPE'] ) {
-		return true;
-	}
-
-	return false;
-
-}
