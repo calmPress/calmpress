434f5da1653e24958cf95185d763757a09b66f97
mark kaplun <mark@marksw.com>
2019-03-03 06:43:21 +0200

    Do not test deprecated function

--- tests/phpunit/tests/pluggable.php
+++ tests/phpunit/tests/pluggable.php
@@ -311,7 +311,6 @@ class Tests_Pluggable extends WP_UnitTestCase {
 					'wp_cache_switch_to_blog'            => array( 'blog_id' ),
 					'wp_cache_add_global_groups'         => array( 'groups' ),
 					'wp_cache_add_non_persistent_groups' => array( 'groups' ),
-					'wp_cache_reset'                     => array(),
 				)
 			);
 		}
