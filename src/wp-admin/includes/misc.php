1ec2f8d5cfbf9d54ce2d1b2a9ca014334d7fd8c2
mark kaplun <mark@marksw.com>
2019-03-08 09:14:51 +0200

    Revert to wordpress branding in the htaccess file as it seems like some plugins are looking for the specific WordPress marker in the htaccess file

--- src/wp-admin/includes/misc.php
+++ src/wp-admin/includes/misc.php
@@ -183,7 +183,7 @@ function save_mod_rewrite_rules() {
 	if ( ( ! file_exists( $htaccess_file ) && is_writable( $home_path ) ) || is_writable( $htaccess_file ) ) {
 		if ( got_mod_rewrite() ) {
 			$rules = explode( "\n", $wp_rewrite->mod_rewrite_rules() );
-			return insert_with_markers( $htaccess_file, 'calmPress', $rules );
+			return insert_with_markers( $htaccess_file, 'WordPress', $rules );
 		}
 	}
 
--- src/wp-admin/options-permalink.php
+++ src/wp-admin/options-permalink.php
@@ -79,7 +79,7 @@ if ( $iis7_permalinks ) {
 		$writable = true;
 	} else {
 		$writable        = false;
-		$existing_rules  = array_filter( extract_from_markers( $home_path . '.htaccess', 'calmPress' ) );
+		$existing_rules  = array_filter( extract_from_markers( $home_path . '.htaccess', 'WordPress' ) );
 		$new_rules       = array_filter( explode( "\n", $wp_rewrite->mod_rewrite_rules() ) );
 		$update_required = ( $new_rules !== $existing_rules );
 	}
