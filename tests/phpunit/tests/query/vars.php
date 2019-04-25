0af66568469a0e61a5267d3d0421e3c8f149b4ef
mark kaplun <mark@marksw.com>
2019-04-25 17:39:11 +0300

    Adjust tests for controlling embedding by an option #246

--- tests/phpunit/tests/query/vars.php
+++ tests/phpunit/tests/query/vars.php
@@ -46,7 +46,6 @@ class Tests_Query_Vars extends WP_UnitTestCase {
 			'preview',
 			'robots',
 			'cpage',
-			'embed',
 			'category_name',
 			'attachment',
 
@@ -69,7 +68,8 @@ class Tests_Query_Vars extends WP_UnitTestCase {
 		sort( $actual );
 		$this->assertEquals( $expected, $actual, 'Care should be taken when introducing new public query vars. See https://core.trac.wordpress.org/ticket/35115' );
 
-		// Test that when feeds are enabled, "feed" is a valid public variable.
+		// Test that when embeding is is enabled, "embed" is a valid public variable.
+		update_option( 'posts_per_rss', 0 );
 		update_option( 'calm_embedding_on', 1 );
 		$this->go_to( '/' );
 		$expected[] = 'embed';
