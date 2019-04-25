86d87a02ca83af3b577fdb3f727498e7337995da
mark kaplun <mark@marksw.com>
2019-04-25 18:30:22 +0300

    Make sure DB settings do not spill from other tests #246

--- tests/phpunit/tests/query/vars.php
+++ tests/phpunit/tests/query/vars.php
@@ -60,6 +60,7 @@ class Tests_Query_Vars extends WP_UnitTestCase {
 		$this->assertEquals( $expected, $actual, 'Care should be taken when introducing new public query vars. See https://core.trac.wordpress.org/ticket/35115' );
 
 		// Test that when feeds are enabled, "feed" is a valid public variable.
+		update_option( 'calm_embedding_on', 0 );
 		update_option( 'posts_per_rss', 5 );
 		$this->go_to( '/' );
 		$expected[] = 'feed';
