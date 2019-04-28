1341603825c9b0c4e530c8004211dac7a12cee44
mark kaplun <mark@marksw.com>
2019-04-28 11:52:38 +0300

    Fix Syntax error

--- src/wp-admin/includes/upgrade.php
+++ src/wp-admin/includes/upgrade.php
@@ -191,7 +191,7 @@ if ( ! function_exists( 'wp_install_defaults' ) ) :
 		$first_comment_url    = ! empty( $first_comment_url ) ? $first_comment_url : 'https://wordpress.org/';
 		$first_comment        = ! empty( $first_comment ) ? $first_comment : __(
 			'Hi, this is a comment.
-To get started with moderating, editing, and deleting comments, please visit the Comments screen in the dashboard.
+To get started with moderating, editing, and deleting comments, please visit the Comments screen in the dashboard.'
 		);
 		$wpdb->insert(
 			$wpdb->comments,
