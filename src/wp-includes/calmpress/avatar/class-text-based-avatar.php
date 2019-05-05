91f0065840a4743903b035fad142b30784702c92
mark kaplun <mark@marksw.com>
2019-05-05 10:01:18 +0300

    remove stray { #88

--- src/wp-includes/calmpress/avatar/class-text-based-avatar.php
+++ src/wp-includes/calmpress/avatar/class-text-based-avatar.php
@@ -111,7 +111,7 @@ class Text_Based_Avatar implements Avatar {
 		}
 		$text = esc_html( $text );
 
-		$html = "<span style='display:inline-block;border-radius:50%;color:white;line-height:${height}px;{width:${width}px;height:${height}px;font-size:${font_size}px;background:${color}'>$text</span>";
+		$html = "<span style='display:inline-block;border-radius:50%;color:white;line-height:${height}px;width:${width}px;height:${height}px;font-size:${font_size}px;background:${color}'>$text</span>";
 
 		/**
 		 * Filters the generated image avatar.
