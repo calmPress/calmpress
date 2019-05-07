e30ccdd831974f51f433db382ae1419e9fc31734
mark kaplun <mark@marksw.com>
2019-05-07 12:58:26 +0300

    js files need to be in the _enqueues directory now since that is where core puts them

--- Gruntfile.js
+++ Gruntfile.js
@@ -227,6 +227,7 @@ module.exports = function(grunt) {
 			'admin-js': {
 				files: {
 					[ WORKING_DIR + 'wp-admin/js/accordion.js' ]: [ './src/js/_enqueues/lib/accordion.js' ],
+					[ WORKING_DIR + 'wp-admin/js/author.js' ]: [ './src/js/_enqueues/admin/author.js' ],
 					[ WORKING_DIR + 'wp-admin/js/code-editor.js' ]: [ './src/js/_enqueues/wp/code-editor.js' ],
 					[ WORKING_DIR + 'wp-admin/js/color-picker.js' ]: [ './src/js/_enqueues/lib/color-picker.js' ],
 					[ WORKING_DIR + 'wp-admin/js/comment.js' ]: [ './src/js/_enqueues/admin/comment.js' ],
