03c9a674ffb188d7eb1d1d930a9de7bc5c08a1f2
mark kaplun <mark@marksw.com>
2020-04-09 06:55:54 +0300

    merge cleanup

--- tests/phpunit/tests/formatting/WPTargetedLinkRel.php
+++ tests/phpunit/tests/formatting/WPTargetedLinkRel.php
@@ -79,17 +79,6 @@ class Tests_Targeted_Link_Rel extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Ensure JSON format is preserved when relation attribute (rel) is missing.
-	 *
-	 * @ticket 46316
-	 */
-	public function test_wp_targeted_link_rel_should_preserve_json() {
-		$content  = '<p>Links: <a href=\"\/\" target=\"_blank\">No rel<\/a><\/p>';
-		$expected = '<p>Links: <a href=\"\/\" target=\"_blank\" rel=\"noopener noreferrer\">No rel<\/a><\/p>';
-		$this->assertEquals( $expected, wp_targeted_link_rel( $content ) );
-	}
-
-	/**
 	 * Ensure correct quotes are used when relation attribute (rel) is missing.
 	 *
 	 * @ticket 47244
