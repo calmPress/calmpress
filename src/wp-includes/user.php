e1783a204c79adcdc6f75410051d9fbe54baf51f
mark kaplun <mark@marksw.com>
2020-04-19 20:42:47 +0300

    return code which most likely fell in merge

--- src/wp-includes/user.php
+++ src/wp-includes/user.php
@@ -1619,6 +1619,18 @@ function wp_insert_user( $userdata ) {
 
 	$user_nicename_check = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->users WHERE user_nicename = %s AND user_login != %s LIMIT 1", $user_nicename, $user_login ) );
 
+	if ( $user_nicename_check ) {
+		$suffix = 2;
+		while ( $user_nicename_check ) {
+			// user_nicename allows 50 chars. Subtract one for a hyphen, plus the length of the suffix.
+			$base_length         = 49 - mb_strlen( $suffix );
+			$alt_user_nicename   = mb_substr( $user_nicename, 0, $base_length ) . "-$suffix";
+			$user_nicename_check = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->users WHERE user_nicename = %s AND user_login != %s LIMIT 1", $alt_user_nicename, $user_login ) );
+			$suffix++;
+		}
+		$user_nicename = $alt_user_nicename;
+	}
+
 	$raw_user_email = empty( $userdata['user_email'] ) ? '' : $userdata['user_email'];
 
 	/**
--- tests/qunit/fixtures/wp-api-generated.js
+++ tests/qunit/fixtures/wp-api-generated.js
@@ -9343,7 +9343,7 @@ mockedApiResponse.TaxonomyModel = {
 
 mockedApiResponse.CategoriesCollection = [
     {
-        "id": 430,
+        "id": 432,
         "count": 0,
         "description": "REST API Client Fixture: Category",
         "link": "http://example.org/?cat=1",
