dd9dfc1e03a1634f19765b12c7874a1e97c3f5c4
mark kaplun <mark@marksw.com>
2020-10-20 19:39:10 +0300

    no ugly urls around here, no need to test

--- tests/phpunit/tests/canonical/sitemaps.php
+++ tests/phpunit/tests/canonical/sitemaps.php
@@ -79,36 +79,4 @@ class Tests_Canonical_Sitemaps extends WP_Canonical_UnitTestCase {
 			array( '/wp-sitemap-taxonomies-category-2.xml', '/wp-sitemap-taxonomies-category-2.xml' ),
 		);
 	}
-
-	/**
-	 * Ensure sitemaps redirects work as expected with ugly permalinks.
-	 *
-	 * @dataProvider data_sitemaps_canonical_ugly_redirects
-	 * @ticket 50910
-	 */
-	public function test_sitemaps_canonical_ugly_redirects( $test_url, $expected ) {
-		$this->set_permalink_structure( '' );
-		$this->assertCanonical( $test_url, $expected, 50910 );
-	}
-
-	/**
-	 * Data provider for test_sitemaps_canonical_ugly_redirects.
-	 *
-	 * @return array[] {
-	 *     Data to test with.
-	 *
-	 *     @type string $0 The test URL.
-	 *     @type string $1 The expected canonical URL.
-	 * }
-	 */
-	public function data_sitemaps_canonical_ugly_redirects() {
-		return array(
-			// Ugly permalinks remain ugly.
-			array( '/?sitemap=index', '/?sitemap=index' ),
-			array( '/?sitemap=posts&sitemap-subtype=post', '/?sitemap=posts&sitemap-subtype=post' ),
-			array( '/?sitemap=posts&sitemap-subtype=post&paged=2', '/?sitemap=posts&sitemap-subtype=post&paged=2' ),
-			array( '/?sitemap=taxonomies&sitemap-subtype=category', '/?sitemap=taxonomies&sitemap-subtype=category' ),
-			array( '/?sitemap=taxonomies&sitemap-subtype=category&paged=2', '/?sitemap=taxonomies&sitemap-subtype=category&paged=2' ),
-		);
-	}
 }
