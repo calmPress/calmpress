dfdab6dd5b2517022d3a320a7e426cec0ee736e0
mark kaplun <mark@marksw.com>
2021-04-22 18:15:25 +0300

    we do not redirect wordpress URL parameters to an actual "pretty" URL

--- tests/phpunit/tests/canonical/postStatus.php
+++ tests/phpunit/tests/canonical/postStatus.php
@@ -212,342 +212,6 @@ class Tests_Canonical_PostStatus extends WP_Canonical_UnitTestCase {
 	}
 
 	/**
-	 * Test canonical redirect does not reveal private posts presence.
-	 *
-	 * @ticket 5272
-	 * @dataProvider data_canonical_redirects_to_plain_permalinks
-	 *
-	 * @param string $post_key  Post key used for creating fixtures.
-	 * @param string $user_role User role.
-	 * @param string $requested Requested URL.
-	 * @param string $expected  Expected URL.
-	 */
-	public function test_canonical_redirects_to_plain_permalinks( $post_key, $user_role, $requested, $expected ) {
-		wp_set_current_user( self::$users[ $user_role ] );
-		$this->set_permalink_structure( '' );
-		$post = self::$posts[ $post_key ];
-		clean_post_cache( $post->ID );
-
-		/*
-		 * The dataProvider runs before the fixures are set up, therefore the
-		 * post object IDs are placeholders that needs to be replaced.
-		 */
-		$requested = str_replace( '%ID%', $post->ID, $requested );
-		$expected  = str_replace( '%ID%', $post->ID, $expected );
-
-		$this->assertCanonical( $requested, $expected );
-	}
-
-	/**
-	 * Data provider for test_canonical_redirects_to_plain_permalinks.
-	 *
-	 * @return array[] Array of arguments for tests {
-	 *     @type string $post_key  Post key used for creating fixtures.
-	 *     @type string $user_role User role.
-	 *     @type string $requested Requested URL.
-	 *     @type string $expected  Expected URL.
-	 * }
-	 */
-	function data_canonical_redirects_to_plain_permalinks() {
-		$data              = array();
-		$all_user_list     = array( 'anon', 'subscriber', 'content_author', 'editor' );
-		$select_allow_list = array( 'content_author', 'editor' );
-		$select_block_list = array( 'anon', 'subscriber' );
-		// All post/page keys
-		$all_user_post_status_keys    = array( 'publish' );
-		$select_user_post_status_keys = array( 'private', 'a-private-status' );
-		$no_user_post_status_keys     = array( 'future', 'draft', 'pending', 'auto-draft' ); // Excludes trash for attachment rules.
-		$select_user_post_type_keys   = array( 'a-public-cpt' );
-		$no_user_post_type_keys       = array( 'a-private-cpt' );
-
-		foreach ( $all_user_post_status_keys as $post_key ) {
-			foreach ( $all_user_list as $user ) {
-				/*
-				 * In the event `redirect_canonical()` is updated to redirect plain permalinks
-				 * to a canonical plain version, these expected values can be changed.
-				 */
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?post_type=page&p=%ID%',
-					'/?post_type=page&p=%ID%',
-				);
-
-				$data[] = array(
-					$post_key,
-					$user,
-					"/?name=$post_key-post",
-					"/?name=$post_key-post",
-				);
-
-				// Ensure rss redirects to rss2.
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					'/?feed=rss2&p=%ID%',
-				);
-
-				// Ensure rss redirects to rss2.
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?feed=rss&page_id=%ID%',
-					'/?feed=rss2&page_id=%ID%',
-				);
-			}
-		}
-
-		foreach ( $select_user_post_status_keys as $post_key ) {
-			foreach ( $select_allow_list as $user ) {
-				/*
-				 * In the event `redirect_canonical()` is updated to redirect plain permalinks
-				 * to a canonical plain version, these expected values can be changed.
-				 */
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?post_type=page&p=%ID%',
-					'/?post_type=page&p=%ID%',
-				);
-
-				$data[] = array(
-					$post_key,
-					$user,
-					"/?name=$post_key-post",
-					"/?name=$post_key-post",
-				);
-
-				// Ensure rss redirects to rss2.
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					'/?feed=rss2&p=%ID%',
-				);
-
-				// Ensure rss redirects to rss2.
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?feed=rss&page_id=%ID%',
-					'/?feed=rss2&page_id=%ID%',
-				);
-			}
-
-			foreach ( $select_block_list as $user ) {
-				/*
-				 * In the event `redirect_canonical()` is updated to redirect plain permalinks
-				 * to a canonical plain version, these expected values MUST NOT be changed.
-				 */
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?post_type=page&p=%ID%',
-					'/?post_type=page&p=%ID%',
-				);
-
-				$data[] = array(
-					$post_key,
-					$user,
-					"/?name=$post_key-post",
-					"/?name=$post_key-post",
-				);
-
-				// Ensure post's existence is not demonstrated by changing rss to rss2.
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					'/?feed=rss&p=%ID%',
-				);
-
-				// Ensure post's existence is not demonstrated by changing rss to rss2.
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?feed=rss&page_id=%ID%',
-					'/?feed=rss&page_id=%ID%',
-				);
-			}
-		}
-
-		foreach ( $no_user_post_status_keys as $post_key ) {
-			foreach ( $all_user_list as $user ) {
-				/*
-				 * In the event `redirect_canonical()` is updated to redirect plain permalinks
-				 * to a canonical plain version, these expected values MUST NOT be changed.
-				 */
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?post_type=page&p=%ID%',
-					'/?post_type=page&p=%ID%',
-				);
-
-				$data[] = array(
-					$post_key,
-					$user,
-					"/?name=$post_key-post",
-					"/?name=$post_key-post",
-				);
-
-				// Ensure post's existence is not demonstrated by changing rss to rss2.
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					'/?feed=rss&p=%ID%',
-				);
-
-				// Ensure post's existence is not demonstrated by changing rss to rss2.
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?feed=rss&page_id=%ID%',
-					'/?feed=rss&page_id=%ID%',
-				);
-			}
-		}
-
-		foreach ( array( 'trash' ) as $post_key ) {
-			foreach ( $all_user_list as $user ) {
-				/*
-				 * In the event `redirect_canonical()` is updated to redirect plain permalinks
-				 * to a canonical plain version, these expected values MUST NOT be changed.
-				 */
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?post_type=page&p=%ID%',
-					'/?post_type=page&p=%ID%',
-				);
-
-				$data[] = array(
-					$post_key,
-					$user,
-					"/?name=$post_key-post",
-					"/?name=$post_key-post",
-				);
-
-				// Ensure post's existence is not demonstrated by changing rss to rss2.
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					'/?feed=rss&p=%ID%',
-				);
-
-				// Ensure post's existence is not demonstrated by changing rss to rss2.
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?feed=rss&page_id=%ID%',
-					'/?feed=rss&page_id=%ID%',
-				);
-			}
-		}
-
-		foreach ( $select_user_post_type_keys as $post_key ) {
-			foreach ( $select_allow_list as $user ) {
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?p=%ID%',
-					'/?a-public-cpt=a-public-cpt',
-				);
-
-				$data[] = array(
-					"$post_key-attachment",
-					$user,
-					'/?attachment_id=%ID%',
-					'/?attachment_id=%ID%',
-				);
-
-				$data[] = array(
-					$post_key,
-					$user,
-					"/?name=$post_key&post_type=$post_key",
-					"/?name=$post_key&post_type=$post_key",
-				);
-
-				// Ensure rss is replaced by rss2.
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					'/?a-public-cpt=a-public-cpt&feed=rss2',
-				);
-			}
-
-			foreach ( $select_block_list as $user ) {
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?p=%ID%',
-					'/?p=%ID%',
-				);
-
-				$data[] = array(
-					"$post_key-attachment",
-					$user,
-					'/?attachment_id=%ID%',
-					'/?attachment_id=%ID%',
-				);
-
-				$data[] = array(
-					$post_key,
-					$user,
-					"/?name=$post_key&post_type=$post_key",
-					"/?name=$post_key&post_type=$post_key",
-				);
-
-				// Ensure rss is not replaced with rss2.
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					'/?feed=rss&p=%ID%',
-				);
-			}
-		}
-
-		foreach ( $no_user_post_type_keys as $post_key ) {
-			foreach ( $all_user_list as $user ) {
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?p=%ID%',
-					'/?p=%ID%',
-				);
-
-				$data[] = array(
-					"$post_key-attachment",
-					$user,
-					'/?attachment_id=%ID%',
-					'/?attachment_id=%ID%',
-				);
-
-				$data[] = array(
-					$post_key,
-					$user,
-					"/?name=$post_key&post_type=$post_key",
-					"/?name=$post_key&post_type=$post_key",
-				);
-
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					'/?feed=rss&p=%ID%',
-				);
-			}
-		}
-
-		return $data;
-	}
-
-	/**
 	 * Test canonical redirect does not reveal private slugs.
 	 *
 	 * @ticket 5272
@@ -606,23 +270,9 @@ class Tests_Canonical_PostStatus extends WP_Canonical_UnitTestCase {
 				);
 
 				$data[] = array(
-					"$post_key-attachment",
-					$user,
-					'/?attachment_id=%ID%',
-					"/$post_key-post/$post_key-inherited-attachment/",
-				);
-
-				$data[] = array(
 					"$post_key-page",
 					$user,
 					'/?post_type=page&p=%ID%',
-					"/$post_key-page/",
-				);
-
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?page_id=%ID%',
 					"/$post_key-page/",
 				);
 
@@ -633,19 +283,6 @@ class Tests_Canonical_PostStatus extends WP_Canonical_UnitTestCase {
 					"/$post_key-post/",
 				);
 
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					"/$post_key-post/feed/",
-				);
-
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?feed=rss&page_id=%ID%',
-					"/$post_key-page/feed/",
-				);
 			}
 		}
 
@@ -659,23 +296,9 @@ class Tests_Canonical_PostStatus extends WP_Canonical_UnitTestCase {
 				);
 
 				$data[] = array(
-					"$post_key-attachment",
-					$user,
-					'/?attachment_id=%ID%',
-					"/$post_key-post/$post_key-inherited-attachment/",
-				);
-
-				$data[] = array(
 					"$post_key-page",
 					$user,
 					'/?post_type=page&p=%ID%',
-					"/$post_key-page/",
-				);
-
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?page_id=%ID%',
 					"/$post_key-page/",
 				);
 
@@ -686,19 +309,6 @@ class Tests_Canonical_PostStatus extends WP_Canonical_UnitTestCase {
 					"/$post_key-post/",
 				);
 
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					"/$post_key-post/feed/",
-				);
-
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?feed=rss&page_id=%ID%',
-					"/$post_key-page/feed/",
-				);
 			}
 
 			foreach ( $select_block_list as $user ) {
@@ -737,19 +347,6 @@ class Tests_Canonical_PostStatus extends WP_Canonical_UnitTestCase {
 					"/?name=$post_key-post",
 				);
 
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					'/?feed=rss&p=%ID%',
-				);
-
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?feed=rss&page_id=%ID%',
-					'/?feed=rss&page_id=%ID%',
-				);
 			}
 		}
 
@@ -763,25 +360,12 @@ class Tests_Canonical_PostStatus extends WP_Canonical_UnitTestCase {
 				);
 
 				$data[] = array(
-					"$post_key-attachment",
-					$user,
-					'/?attachment_id=%ID%',
-					"/$post_key/$post_key/$post_key-inherited-attachment/",
-				);
-
-				$data[] = array(
 					$post_key,
 					$user,
 					"/?name=$post_key&post_type=$post_key",
 					"/$post_key/$post_key/?post_type=$post_key",
 				);
 
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					"/$post_key/$post_key/feed/",
-				);
 			}
 
 			foreach ( $select_block_list as $user ) {
@@ -806,12 +390,6 @@ class Tests_Canonical_PostStatus extends WP_Canonical_UnitTestCase {
 					"/?name=$post_key&post_type=$post_key",
 				);
 
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					'/?feed=rss&p=%ID%',
-				);
 			}
 		}
 
@@ -839,12 +417,6 @@ class Tests_Canonical_PostStatus extends WP_Canonical_UnitTestCase {
 					"/?name=$post_key&post_type=$post_key",
 				);
 
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					'/?feed=rss&p=%ID%',
-				);
 			}
 		}
 
@@ -885,19 +457,6 @@ class Tests_Canonical_PostStatus extends WP_Canonical_UnitTestCase {
 					"/?name=$post_key-post",
 				);
 
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					'/?feed=rss&p=%ID%',
-				);
-
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?feed=rss&page_id=%ID%',
-					'/?feed=rss&page_id=%ID%',
-				);
 			}
 		}
 
@@ -952,19 +511,6 @@ class Tests_Canonical_PostStatus extends WP_Canonical_UnitTestCase {
 					"/?name=$post_key-post",
 				);
 
-				$data[] = array(
-					$post_key,
-					$user,
-					'/?feed=rss&p=%ID%',
-					'/?feed=rss&p=%ID%',
-				);
-
-				$data[] = array(
-					"$post_key-page",
-					$user,
-					'/?feed=rss&page_id=%ID%',
-					'/?feed=rss&page_id=%ID%',
-				);
 			}
 		}
 
