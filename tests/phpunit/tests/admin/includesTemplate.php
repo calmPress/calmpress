2c5f682160975f1d80b56fb828bb28390e70a17a
mark kaplun <mark@marksw.com>
2021-04-23 13:16:19 +0300

    we don't have browser nag and quick press, so don't test them

--- tests/phpunit/tests/admin/includesTemplate.php
+++ tests/phpunit/tests/admin/includesTemplate.php
@@ -213,13 +213,6 @@ class Tests_Admin_includesTemplate extends WP_UnitTestCase {
 			require_once ABSPATH . 'wp-admin/includes/dashboard.php';
 		}
 
-		// Some hardcoded defaults for core widgets.
-		wp_add_dashboard_widget( 'dashboard_quick_press', 'Quick', '__return_false' );
-		wp_add_dashboard_widget( 'dashboard_browser_nag', 'Nag', '__return_false' );
-
-		$this->assertArrayHasKey( 'dashboard_quick_press', $wp_meta_boxes['dashboard']['side']['core'] );
-		$this->assertArrayHasKey( 'dashboard_browser_nag', $wp_meta_boxes['dashboard']['normal']['high'] );
-
 		// Location and priority defaults.
 		wp_add_dashboard_widget( 'dashboard1', 'Widget 1', '__return_false', null, null, 'foo' );
 		wp_add_dashboard_widget( 'dashboard2', 'Widget 2', '__return_false', null, null, null, 'bar' );
@@ -228,8 +221,6 @@ class Tests_Admin_includesTemplate extends WP_UnitTestCase {
 		$this->assertArrayHasKey( 'dashboard2', $wp_meta_boxes['dashboard']['normal']['bar'] );
 
 		// Cleanup.
-		remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
-		remove_meta_box( 'dashboard_browser_nag', 'dashboard', 'normal' );
 		remove_meta_box( 'dashboard1', 'dashboard', 'foo' );
 
 		// This doesn't actually get removed due to the invalid priority.
