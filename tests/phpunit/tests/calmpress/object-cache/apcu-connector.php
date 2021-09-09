6b8241782dccda89da31c68efc6b0f714cf24a85
mark kaplun <mark@marksw.com>
2021-09-09 16:55:51 +0300

    mock apcu_enabled instead of apcu_store of testing #305

--- tests/phpunit/tests/calmpress/object-cache/apcu-connector.php
+++ tests/phpunit/tests/calmpress/object-cache/apcu-connector.php
@@ -11,6 +11,18 @@ declare(strict_types=1);
 use calmpress\object_cache\APCu_Connector;
 use calmpress\object_cache\APCu;
 
+// Protect against redefinition as this code is also used in the apcu specific testing.
+if ( ! function_exists( 'apcu_enabled' ) ) {
+	global $calmpress_apcu_enabled;
+	$calmpress_apcu_enabled = false;
+
+	function apcu_enabled(): bool {
+		global $calmpress_apcu_enabled;
+
+		return $calmpress_apcu_enabled;
+	}
+}
+
 /**
  * Test the APCu_Connector
  *
@@ -18,7 +30,7 @@ use calmpress\object_cache\APCu;
  *
  * @since 1.0.0
  */
-class WP_Test_Chained_Caches extends WP_UnitTestCase {
+class WP_Test_APCu_Connector extends WP_UnitTestCase {
 
 	/**
 	 * APCu_is_avaialable should return false when APCu is not active.
@@ -48,37 +60,36 @@ class WP_Test_Chained_Caches extends WP_UnitTestCase {
 	 * Test APCu_is_avaialable when mocked APCu is around.
 	 */
 	public function test_APCu_is_avaialable() {
+		global $calmpress_apcu_enabled;
+		$calmpress_apcu_enabled = true;
 
-		// mock APCu detection to be able to create objects
-		function apcu_store() {};
 		$this->assertTrue( APCu_Connector::APCu_is_avaialable() );
+		$calmpress_apcu_enabled = false;
 	}
 
 	/**
 	 * Test namespace returns the namespace with which the object was created.
 	 */
 	public function test_namespace() {
+		global $calmpress_apcu_enabled;
+		$calmpress_apcu_enabled = true;
 
 		$connector = new APCu_Connector( 'test' );
 
 		$this->assertSame( 'test', $connector->namespace() );
+		$calmpress_apcu_enabled = false;
 	}
 
 	/**
 	 * Test APCu global group cache creation.
 	 */
-	public function test_create_global_group_cache() {
-		$connector = new APCu_Connector( 'test' );
-		$cache     = $connector->create_global_group_cache( 'test' );
-		$this->assertTrue( is_a( $cache, '\calmpress\object_cache\APCu' ) );
-	}
+	public function test_create_cache() {
+		global $calmpress_apcu_enabled;
+		$calmpress_apcu_enabled = true;
 
-	/**
-	 * Test APCu blog group cache creation.
-	 */
-	public function test_create_blog_group_cache() {
 		$connector = new APCu_Connector( 'test' );
-		$cache     = $connector->create_blog_group_cache( 1, 'test' );
+		$cache     = $connector->create_cache( 'test' );
 		$this->assertTrue( is_a( $cache, '\calmpress\object_cache\APCu' ) );
+		$calmpress_apcu_enabled = false;
 	}
 }
