86ebd11660eacd221ead5382792cfe0b7bc5fdea
mark kaplun <mark@marksw.com>
2021-09-11 22:06:32 +0300

    implement php file caching #309

---
+++ tests/phpunit/tests/calmpress/object-cache/php-file.php
@@ -0,0 +1,224 @@
+<?php
+/**
+ * Unit tests covering PHP file object cache functionality.
+ *
+ * @package calmPress
+ * @since 1.0.0
+ */
+
+declare(strict_types=1);
+
+use calmpress\object_cache\PHP_File;
+
+/**
+ * Mock the key validation functions for testing
+ *
+ * Since we are mocking static function, zero the state on each object creation.
+ *
+ * @since 1.0.0
+ */
+class Mock_PHP_File extends PHP_File {
+
+	// Conter for how many times throw_if_not_string was called.
+	public static int $validation_key_called = 0;
+
+	// Conter for how many times throw_if_not_iterable was called.
+	public static int $validation_iterable_called = 0;
+
+	// Counter for how many times throw_if_not_iterable was called.
+	public static int $ttl_called = 0;
+
+	/**
+	 * Zero out the counter as we start new test.
+	 */
+	public function __construct( string $path) {
+		parent::__construct( $path );
+		self::$validation_key_called = 0;
+		self::$validation_iterable_called = 0;
+		self::$ttl_called = 0;
+	}
+
+	protected static function throw_if_not_string_int( $key ) {
+		self::$validation_key_called++;
+	}
+
+	protected static function throw_if_not_iterable( $keys, bool $check_values = true ) {
+		self::$validation_iterable_called++;
+	}
+
+	protected static function ttl_to_seconds( $ttl ): int {
+		self::$ttl_called++;
+		return 0;
+	}
+
+	/**
+	 * Mock opcache to be enabled.
+	 */
+	public static function opcahce_enabled():bool {
+		return true;
+	}
+}
+
+/**
+ * Mock the function opcache_invalidate to avoid errors and see if the correct file
+ * is invalidated.
+ */
+function opcache_invalidate( string $file ) {
+	global $opcache_invalidate_file;
+
+	$opcache_invalidate_file = $file;
+}
+
+/**
+ * Tests for the PHP_File class.
+ * 
+ * @since 1.0.0
+ */
+class WP_Test_PHP_File extends WP_UnitTestCase {
+
+	/**
+	 * Test that get returns default when key not in cache and calls key validation.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_get_uses_default_when_not_exist_or_not_valid() {
+		$cache = new Mock_PHP_File( 'test/sub' );
+
+		// File do not exist after initialization.
+		$this->assertSame( 'test', $cache->get( 'key', 'test' ) );
+		$this->assertSame( 1, Mock_PHP_File::$validation_key_called );
+
+		// File contain "garbage".
+		file_put_contents( PHP_File::CACHE_ROOT_DIR . 'test/sub/key.php', 'garbage' );
+		$this->assertSame( 'test', $cache->get( 'key', 'test' ) );
+
+		// File contain php with syntax error.
+		file_put_contents( PHP_File::CACHE_ROOT_DIR . 'test/sub/key.php', '<?php return [' .( time() + 1000 ) . ', 45]' );
+		$this->assertSame( 'test', $cache->get( 'key', 'test' ) );
+
+		// File contains expired value.
+		file_put_contents( PHP_File::CACHE_ROOT_DIR . 'test/sub/key.php', '<?php return [' . ( time() - 1000 ) . ', 45];' );
+		$this->assertSame( 'test', $cache->get( 'key', 'test' ) );
+	}
+
+	/**
+	 * Test that files created with a value of a specific type can be properly read.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_get_with_some_types() {
+		$cache = new Mock_PHP_File( 'test/sub' );
+
+		// File contains integer.
+		file_put_contents( PHP_File::CACHE_ROOT_DIR . 'test/sub/key.php', '<?php return [' . ( time() + 1000 ) . ', 45];' );
+		$this->assertSame( 45, $cache->get( 'key', 'test' ) );
+
+		// File contains string.
+		file_put_contents( PHP_File::CACHE_ROOT_DIR . 'test/sub/key.php', '<?php return [' . ( time() + 1000 ) . ', "str"];' );
+		$this->assertSame( 'str', $cache->get( 'key', 'test' ) );
+
+		// File contains array.
+		file_put_contents( PHP_File::CACHE_ROOT_DIR . 'test/sub/key.php', '<?php return [' . ( time() + 1000 ) . ', [1, "str"]];' );
+		$this->assertSame( [1, 'str'], $cache->get( 'key', 'test' ) );
+	}
+
+	/**
+	 * Test that set works and calles key validation.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_set() {
+		$cache = new Mock_PHP_File( 'test/sub' );
+		$cache->set( 'key', 'value' );
+		$this->assertSame( 1, Mock_PHP_File::$validation_key_called );
+		$this->assertSame( 1, Mock_PHP_File::$ttl_called );
+		$this->assertSame( 'value', $cache->get( 'key', 'test' ) );
+	}
+
+	/**
+	 * Test that has works and calles key validation.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_has() {
+		$cache = new Mock_PHP_File( 'test/sub' );
+		@unlink( PHP_File::CACHE_ROOT_DIR . 'test/sub/key.php' );
+		$this->assertFalse( $cache->has( 'key' ) );
+
+		// two validation calls, one for the delete and one for the has.
+		$this->assertSame( 2, Mock_PHP_File::$validation_key_called );
+		$cache->set( 'key', 'value' );
+		$this->assertTrue( $cache->has( 'key' ) );
+	}
+
+	/**
+	 * Test that delete works and calles key validation.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_delete() {
+		global $opcache_invalidate_file;
+
+		$cache = new Mock_PHP_File( 'test/sub' );
+		$cache->set( 'key', 'value' );
+		$cache->delete( 'key' );
+
+		// validation called twice, first for set, than for delete.
+		$this->assertSame( 2, Mock_PHP_File::$validation_key_called );
+		$this->assertFalse( $cache->has( 'key' ) );
+		$this->assertSame( PHP_File::CACHE_ROOT_DIR . 'test/sub/key.php', $opcache_invalidate_file );
+	}
+
+	/**
+	 * Test that getMultiple returns default when key not in cache and validates keys.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_getMultiple_uses_default_when_not_exist() {
+		$cache = new Mock_PHP_File( 'test/sub' );
+		$this->assertSame( ['key1'=>'test', 'key2'=>'test'] , $cache->getMultiple( ['key1', 'key2'], 'test' ) );
+		$this->assertSame( 1, Mock_PHP_File::$validation_iterable_called );
+	}
+
+	/**
+	 * Test that setMultiple add the values to the cache and validates keys.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_setMultiple() {
+		$cache = new Mock_PHP_File( 'test/sub' );
+		$cache->setMultiple( ['key1' => 'value1', 'key2' => 'value2'] );
+		$this->assertSame( 1, Mock_PHP_File::$validation_iterable_called );
+		$this->assertSame( 1, Mock_PHP_File::$ttl_called );
+		$this->assertSame( ['key1' => 'value1', 'key2' => 'value2'] , $cache->getMultiple( ['key1', 'key2'], 'test' ) );
+	}
+
+	/**
+	 * Test that deleteMultiple deletes the values from the cache  and validates keys.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_deleteMultiple() {
+		$cache = new Mock_PHP_File( 'test/sub' );
+		$cache->setMultiple( ['key1' => 'value1', 'key2' => 'value2'] );
+		$cache->deleteMultiple( ['key1', 'key2'] );
+
+		// validation called twice, first for set, than for delete.
+		$this->assertSame( 2, Mock_PHP_File::$validation_iterable_called );
+		$this->assertFalse( $cache->has( 'key1' ) );
+		$this->assertFalse( $cache->has( 'key2' ) );
+	}
+
+	/**
+	 * Test that clear deletes the cache.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_clear() {
+		$cache = new Mock_PHP_File( 'test/sub' );
+		$cache->setMultiple( ['key1' => 'value1', 'key2' => 'value2'] );
+		$cache->clear();
+		$this->assertFalse( $cache->has( 'key1' ) );
+		$this->assertFalse( $cache->has( 'key2' ) );
+	}
+}
