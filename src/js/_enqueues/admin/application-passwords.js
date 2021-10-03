9e225a83b6649243ca34c87a37fb3d3270d42432
mark kaplun <mark@marksw.com>
2021-10-03 14:48:37 +0300

    make application password management look like the rest of user management screen

--- src/js/_enqueues/admin/application-passwords.js
+++ src/js/_enqueues/admin/application-passwords.js
@@ -8,7 +8,7 @@
 		$newAppPassField = $newAppPassForm.find( '.input' ),
 		$newAppPassButton = $newAppPassForm.find( '.button' ),
 		$appPassTwrapper = $appPassSection.find( '.application-passwords-list-table-wrapper' ),
-		$appPassTbody = $appPassSection.find( 'tbody' ),
+		$appPassTbody = $appPassTwrapper.find( 'tbody' ),
 		$appPassTrNoItems = $appPassTbody.find( '.no-items' ),
 		$removeAllBtn = $( '#revoke-all-application-passwords' ),
 		tmplNewAppPass = wp.template( 'new-application-password' ),
