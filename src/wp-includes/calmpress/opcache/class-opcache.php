f0b17824cfce217754e922bac71cf7540ad11248
mark kaplun <mark@marksw.com>
2021-10-06 09:22:45 +0300

    fix bad brain fart

--- src/wp-includes/calmpress/opcache/class-opcache.php
+++ src/wp-includes/calmpress/opcache/class-opcache.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Implementation of stats holder for opcache.
+ * Implementation of a connector to PHP's opcode cache.
  *
  * @package calmPress
  * @since 1.0.0
@@ -11,116 +11,76 @@ declare(strict_types=1);
 namespace calmpress\opcache;
 
 /**
- * An object of the class holds a sample in specific time (object's creation time) of the opcache stats.
+ * A utility class to query and change the state of the opcache.
  *
  * @since 1.0.0
  */
-class Stats {
+class Opcache {
 
 	/**
-	 * Holds the stats data set at object creation.
+	 * Construct a connector to the opcache fuctionality.
 	 *
 	 * @since 1.0.0
 	 *
-	 * @var array
+	 * @throws \RuntimeException if opcache API is not available.
 	 */
-	private array $stats = [];
+	public function __construct() {
 
-	/**
-	 * Create a stats object with the stats at the time of creation.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param array $stats. An array structured like what opcache_get_status returns.
-	 */
-	public function __construct( array $stats ) {
-		$this->stats = $stats;
+		if ( ! static::api_is_available() ) {
+			throw new \RuntimeException( 'Opcache API is not available' );
+		}
 	}
 
 	/**
-	 * Get last restart of the opcache in unix seconds.
+	 * Get the opcache stats.
 	 *
 	 * @since 1.0.0
 	 *
-	 * @return int The unix time of the last restart.
+	 * @return Stats An object serving as an interface to get the stats.
 	 */
-	public function last_restart_time(): int {
-		$ret = $this->stats['opcache_statistics']['last_restart_time'];
+	public function stats(): Stats {
+		return new Stats( opcache_get_status( false ) );
+	}
 
-		// If the value is 0, the opcache did not restart since server restarted therefor
-		// fetch the server start time.
-		if ( 0 === $ret ) {
-			$ret = $this->stats['opcache_statistics']['start_time'];
+	/**
+	 * Reset the opcache.
+	 *
+	 * @since 1.0.0
+	 */
+	public function reset() {
+		opcache_reset();
+	}
+
+	/**
+	 * Signal to the opcache to invalidate a file in the cache.
+	 *
+	 * It is the calling code responsability to make sure the api is available.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $file The path of the file to invalidate.
+	 */
+	public static function invalidate_file( string $file ) {
+		opcache_invalidate( $file, true );
+	}
+
+	/**
+	 * Check if the Opcache API can be used.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return bool true if it can, otherwise false.
+	 */
+	public static function api_is_available() : bool {
+		if ( function_exists( 'opcache_get_status' ) ) {
+			// This check ensures that it is possible to use the api to do stuff,
+			// especially invalidate cache files. Without invalidation there is
+			// a potential of using stale values.
+			if ( false !== opcache_get_status( false ) ) {	
+				return true;
+			}
 		}
 
-		return $ret;
-	}
-
-	/**
-	 * Get total hits.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return int The number of total hits.
-	 */
-	public function hits(): int {
-		return $this->stats['opcache_statistics']['hits'];
-	}
-
-	/**
-	 * Number of system initiated restarts. Lamps together out of memory and out of cache.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return int The number restarts.
-	 */
-	public function system_restarts(): int {
-		return $this->stats['opcache_statistics']['oom_restarts'] + $this->stats['opcache_statistics']['hash_restarts'];
-	}
-
-	/**
-	 * Number of initiated restarts via the API.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return int The number restarts.
-	 */
-	public function external_restarts(): int {
-		return $this->stats['opcache_statistics']['manual_restarts'];
-	}
-
-	/**
-	 * Get miss rate.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return float The miss rate.
-	 */
-	public function miss_rate(): float {
-		return 100.0 - $this->stats['opcache_statistics']['opcache_hit_rate'];
-	}
-
-	/**
-	 * The precentage of cached keys used out of the max possible number.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return float The precentage.
-	 */
-	public function cached_keys_usage(): float {
-		return $this->stats['opcache_statistics']['num_cached_keys'] / $this->stats['opcache_statistics']['max_cached_keys'] * 100.0;
-	}
-
-	/**
-	 * The precentage of memory used out of the max possible.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return float The precentage.
-	 */
-	public function memory_usage(): float {
-		$used  = $this->stats['memory_usage']['used_memory'] + $this->stats['memory_usage']['wasted_memory'];
-		$total = $used + $this->stats['memory_usage']['free_memory'];
-		return $used / $total * 100.0;
+		return false;
 	}
 }
