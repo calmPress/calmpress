751ce2cffdf401d47a9871bc9863fd8ebf7ab549
mark kaplun <mark@marksw.com>
2022-04-21 16:04:11 +0300

    test against the default theme

--- tests/phpunit/tests/ajax/ManageThemes.php
+++ tests/phpunit/tests/ajax/ManageThemes.php
@@ -108,7 +108,7 @@ class Tests_Ajax_Manage_Themes extends WP_Ajax_UnitTestCase {
 		$this->_setRole( 'administrator' );
 
 		$_POST['_ajax_nonce'] = wp_create_nonce( 'updates' );
-		$_POST['slug']        = 'twentyten';
+		$_POST['slug']        = 'calmseventeen';
 
 		// Make the request.
 		try {
@@ -125,12 +125,12 @@ class Tests_Ajax_Manage_Themes extends WP_Ajax_UnitTestCase {
 		// Get the response.
 		$response = json_decode( $this->_last_response, true );
 
-		$theme    = wp_get_theme( 'twentyten' );
+		$theme    = wp_get_theme( 'calmseventeen' );
 		$expected = array(
 			'success' => false,
 			'data'    => array(
 				'update'       => 'theme',
-				'slug'         => 'twentyten',
+				'slug'         => 'calmseventeen',
 				'oldVersion'   => $theme->get( 'Version' ),
 				'newVersion'   => '',
 				'debug'        => array( 'The theme is at the latest version.' ),
