1d75a7566d4923c241c014f3a26ede7aa658357b
mark kaplun <mark@marksw.com>
2024-12-10 07:44:54 +0200

    unit tests for base64URL encoding and decoding

--- tests/phpunit/tests/calmpress/utils.php
+++ tests/phpunit/tests/calmpress/utils.php
@@ -8,6 +8,9 @@
 
 use calmpress\utils;
 
+use function calmpress\utils\base64URL_decode;
+use function calmpress\utils\base64URL_encode;
+
 class WP_Test_Utils extends WP_UnitTestCase {
 
 	/**
@@ -32,5 +35,25 @@ class WP_Test_Utils extends WP_UnitTestCase {
 		// Confirm that it's still only enqueued once
 		$inline_styles = $wp_styles->get_data( 'handle', 'after' );
 		$this->assertCount( 1, $inline_styles );
+	}
+
+	/**
+	 * Test base64URL decode an encode
+	 * 
+	 * @since 1.0.0
+	 */
+	function test_base64url() {
+
+		// simple string
+		$en = base64URL_encode( 'dummy' );
+		$this->assertSame( 'dummy', base64URL_decode( $en ) );
+
+		// A string with some invalid URL characters when base64 encoded.
+		$en = base64URL_encode( "\xFA\xFB\xF" );
+		$this->assertSame( "\xFA\xFB\xF", base64URL_decode( $en ) );
+
+		// String not matching base64URL format.
+		$de = base64URL_decode( 'pQECAyYgASFYIF6t9Oa1Z3ZL3mYjiHU5j9z6Bk3j+8m5UwxyZZ9S_ZUIlgg==INVALID==' );
+		$this->assertFalse( $de );
 	}
 }
