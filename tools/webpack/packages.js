7fbc28d860be50ace9842c338e4daacb467a626d
mark kaplun <mark@marksw.com>
2025-06-25 14:33:35 +0300

    do not generate block related php files

--- tools/webpack/packages.js
+++ tools/webpack/packages.js
@@ -99,9 +99,6 @@ module.exports = function( env = { environment: 'production', watch: false, buil
 	};
 
 	const phpFiles = {
-		'block-serialization-default-parser/class-wp-block-parser.php': 'wp-includes/class-wp-block-parser.php',
-		'block-serialization-default-parser/class-wp-block-parser-frame.php': 'wp-includes/class-wp-block-parser-frame.php',
-		'block-serialization-default-parser/class-wp-block-parser-block.php': 'wp-includes/class-wp-block-parser-block.php',
 	};
 
 	const developmentCopies = mapVendorCopies( vendors, buildTarget );
