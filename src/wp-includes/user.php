7dc186aa2d18579c47a80248645fdf811234116c
mark kaplun <mark@marksw.com>
2025-07-16 18:44:03 +0300

    change text to have a more true description of what passwords a user can set

--- src/wp-includes/user.php
+++ src/wp-includes/user.php
@@ -2361,7 +2361,7 @@ function _wp_get_user_contactmethods( $user = null ) {
  * @return string The password hint text.
  */
 function wp_get_password_hint() {
-	$hint = __( 'Hint: The password should be at least twelve characters long. To make it stronger, use upper and lower case letters, numbers, and symbols like ! " ? $ % ^ &amp; ).' );
+	$hint = __( 'You can use any password, but strong passwords are recommended: at least 12 characters, with a mix of letters, numbers, and symbols.' );
 
 	/**
 	 * Filters the text describing the site's password complexity policy.
