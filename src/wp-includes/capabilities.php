76894350bdac763a2aa56c7d37e5936962c26125
mark kaplun <mark@marksw.com>
2025-07-18 20:37:05 +0300

    fix careless deletion

--- src/wp-includes/capabilities.php
+++ src/wp-includes/capabilities.php
@@ -517,6 +517,8 @@ function map_meta_cap( $cap, $user_id, ...$args ) {
 			break;
 		case 'export_others_personal_data':
 		case 'erase_others_personal_data':
+			$caps[] = is_multisite() ? 'manage_network' : 'manage_options';
+			break;
 		case 'create_app_password':
 		case 'list_app_passwords':
 		case 'read_app_password':
