6ff7bf5202d86345be8ba56896442042e3dfdd9c
mark kaplun <mark@marksw.com>
2026-01-19 19:55:49 +0200

    show the discussion meta box by default

--- src/wp-admin/includes/screen.php
+++ src/wp-admin/includes/screen.php
@@ -169,7 +169,7 @@ function get_hidden_meta_boxes( $screen ) {
 
 		if ( 'post' === $screen->base ) {
 			if ( in_array( $screen->post_type, array( 'post', 'page', 'attachment' ), true ) ) {
-				$hidden = array('slugdiv', 'postexcerpt', 'commentstatusdiv', 'commentsdiv', 'authordiv', 'revisionsdiv');
+				$hidden = array('slugdiv', 'postexcerpt', 'commentsdiv', 'authordiv', 'revisionsdiv');
 			} else {
 				$hidden = array( 'slugdiv' );
 			}
