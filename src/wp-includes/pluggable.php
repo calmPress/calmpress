757290c643482b39591b7dcbe65a1968b8921589
mark kaplun <mark@marksw.com>
2026-06-06 17:39:03 +0300

    adjust wp_mail for split email delivery options #428

--- src/wp-includes/pluggable.php
+++ src/wp-includes/pluggable.php
@@ -397,14 +397,15 @@ if ( ! function_exists( 'wp_mail' ) ) :
 		 */
 		$phpmailer->Encoding = PHPMailer\PHPMailer\PHPMailer::ENCODING_8BIT;
 
-		$options = get_option( 'calm_email_delivery' );
+		$options    = get_option( 'calm_email_transport' );
+		$id_options = get_option( 'calm_email_preferences' );
 
 		// Set "From" name and email.
 
 		// If we don't have a name from the input headers.
 		if ( ! isset( $from_name ) ) {
-			if ( $options['from_name'] ) {
-				$from_name = $options['from_name'];
+			if ( $id_options['from_name'] ) {
+				$from_name = $id_options['from_name'];
 			} else {
 				$from_name = 'calmPress';
 			}
@@ -646,8 +647,8 @@ if ( ! function_exists( 'wp_mail' ) ) :
 		try {
 			$send = $phpmailer->send();
 
-			if ( $options['verbosity'] !== 'no' ) {
-				\calmpress\logger\Log_Emails::mail_success( $phpmailer, $options['verbosity'] );
+			if ( $id_options['verbosity'] !== 'no' ) {
+				\calmpress\logger\Log_Emails::mail_success( $phpmailer, $id_options['verbosity'] );
 			}
 
 			/**
