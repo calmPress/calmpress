d00938eadaad67f5cce0521b0ba6d880c394695e
mark kaplun <mark@marksw.com>
2026-09-19 20:03:37 +0300

    easier paths to load application password management

---
+++ src/wp-admin/network/application-passwords.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Network Application Passwords administration screen.
+ *
+ * @package CalmPress
+ * @subpackage Multisite
+ * @since calmPress 1.0.0
+ */
+
+/** Load the Network Administration bootstrap. */
+require_once __DIR__ . '/admin.php';
+
+require ABSPATH . 'wp-admin/application-passwords.php';
---
+++ src/wp-admin/user/application-passwords.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User Dashboard Application Passwords administration screen.
+ *
+ * @package CalmPress
+ * @subpackage Administration
+ * @since calmPress 1.0.0
+ */
+
+/** Load the User Dashboard administration bootstrap. */
+require_once __DIR__ . '/admin.php';
+
+require ABSPATH . 'wp-admin/application-passwords.php';
