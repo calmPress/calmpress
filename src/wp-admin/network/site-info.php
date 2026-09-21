1ce92cc2edf8bb15b5e22016465407297cb76c94
mark kaplun <mark@marksw.com>
2026-09-21 20:50:37 +0300

    improve site edit to enable a more explicit domain mapping

--- src/wp-admin/network/site-info.php
+++ src/wp-admin/network/site-info.php
@@ -29,81 +29,102 @@ if ( ! can_edit_network( $details->site_id ) ) {
 	wp_die( __( 'Sorry, you are not allowed to access this page.' ), 403 );
 }
 
-$parsed_scheme = parse_url( $details->siteurl, PHP_URL_SCHEME );
-$is_main_site  = is_main_site( $id );
+$parsed_scheme      = parse_url( $details->siteurl, PHP_URL_SCHEME );
+$is_main_site       = is_main_site( $id );
+$site_url_error     = '';
+$submitted_site_url = '';
 
 if ( isset( $_REQUEST['action'] ) && 'update-site' === $_REQUEST['action'] ) {
 	check_admin_referer( 'edit-site' );
 
-	switch_to_blog( $id );
-
-	// Rewrite rules can't be flushed during switch to blog.
-	delete_option( 'rewrite_rules' );
-
-	$blog_data           = wp_unslash( $_POST['blog'] );
-	$blog_data['scheme'] = $parsed_scheme;
+	$blog_data           = isset( $_POST['blog'] ) ? wp_unslash( $_POST['blog'] ) : array();
+	$blog_data['scheme'] = 'https';
 
 	if ( $is_main_site ) {
 		// On the network's main site, don't allow the domain or path to change.
 		$blog_data['domain'] = $details->domain;
 		$blog_data['path']   = $details->path;
 	} else {
-		// For any other site, the scheme, domain, and path can all be changed. We first
-		// need to ensure a scheme has been provided, otherwise fallback to the existing.
-		$new_url_scheme = parse_url( $blog_data['url'], PHP_URL_SCHEME );
+		$submitted_site_url = trim( $blog_data['url'] ?? '' );
 
-		if ( ! $new_url_scheme ) {
-			$blog_data['url'] = esc_url( $parsed_scheme . '://' . $blog_data['url'] );
-		}
-		$update_parsed_url = parse_url( $blog_data['url'] );
-
-		// If a path is not provided, use the default of `/`.
-		if ( ! isset( $update_parsed_url['path'] ) ) {
-			$update_parsed_url['path'] = '/';
+		if ( str_starts_with( strtolower( $submitted_site_url ), 'https://' ) ) {
+			$submitted_site_url = substr( $submitted_site_url, 8 );
+		} elseif ( preg_match( '#^[a-z][a-z0-9+.-]*://#i', $submitted_site_url ) ) {
+			$site_url_error = esc_html__( 'Enter a valid site address.' );
 		}
 
-		$blog_data['scheme'] = $update_parsed_url['scheme'];
+		$update_parsed_url = wp_parse_url( 'https://' . $submitted_site_url );
 
-		// Make sure to not lose the port if it was provided.
-		$blog_data['domain'] = $update_parsed_url['host'];
-		if ( isset( $update_parsed_url['port'] ) ) {
-			$blog_data['domain'] .= ':' . $update_parsed_url['port'];
+		if (
+			! $site_url_error
+			&& ( false === $update_parsed_url
+			|| ! filter_var( $update_parsed_url['host'] ?? '', FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME )
+			|| isset( $update_parsed_url['user'] )
+			|| isset( $update_parsed_url['pass'] )
+			|| isset( $update_parsed_url['query'] )
+			|| isset( $update_parsed_url['fragment'] ) )
+		) {
+			$site_url_error = esc_html__( 'Enter a valid site address.' );
 		}
 
-		$blog_data['path'] = $update_parsed_url['path'];
+		if ( ! $site_url_error ) {
+			// If a path is not provided, use the default of `/`.
+			if ( ! isset( $update_parsed_url['path'] ) ) {
+				$update_parsed_url['path'] = '/';
+			}
+
+			// Make sure to not lose the port if it was provided.
+			$blog_data['domain'] = $update_parsed_url['host'];
+			if ( isset( $update_parsed_url['port'] ) ) {
+				$blog_data['domain'] .= ':' . $update_parsed_url['port'];
+			}
+
+			$blog_data['path'] = $update_parsed_url['path'];
+		}
 	}
 
-	$existing_details     = get_site( $id );
+	if ( ! $site_url_error ) {
+		switch_to_blog( $id );
 
-	update_blog_details( $id, $blog_data );
+		// Rewrite rules can't be flushed during switch to blog.
+		delete_option( 'rewrite_rules' );
 
-	// Maybe update home and siteurl options.
-	$new_details = get_site( $id );
+		$existing_details = get_site( $id );
 
-	$old_home_url    = trailingslashit( esc_url( get_option( 'home' ) ) );
-	$old_home_parsed = parse_url( $old_home_url );
-	$old_home_host   = $old_home_parsed['host'] . ( isset( $old_home_parsed['port'] ) ? ':' . $old_home_parsed['port'] : '' );
+		update_blog_details( $id, $blog_data );
 
-	if ( $old_home_host === $existing_details->domain && $old_home_parsed['path'] === $existing_details->path ) {
-		$new_home_url = untrailingslashit( sanitize_url( $blog_data['scheme'] . '://' . $new_details->domain . $new_details->path ) );
-		update_option( 'home', $new_home_url );
+		// Maybe update home and siteurl options.
+		$new_details = get_site( $id );
+		$new_url     = untrailingslashit( sanitize_url( 'https://' . $new_details->domain . $new_details->path ) );
+
+		$old_home_url    = trailingslashit( esc_url( get_option( 'home' ) ) );
+		$old_home_parsed = parse_url( $old_home_url );
+		$old_home_host   = $old_home_parsed['host'] . ( isset( $old_home_parsed['port'] ) ? ':' . $old_home_parsed['port'] : '' );
+
+		if ( $old_home_host === $existing_details->domain && $old_home_parsed['path'] === $existing_details->path ) {
+			update_option( 'home', $new_url );
+		}
+
+		$old_site_url    = trailingslashit( esc_url( get_option( 'siteurl' ) ) );
+		$old_site_parsed = parse_url( $old_site_url );
+		$old_site_host   = $old_site_parsed['host'] . ( isset( $old_site_parsed['port'] ) ? ':' . $old_site_parsed['port'] : '' );
+
+		if ( $old_site_host === $existing_details->domain && $old_site_parsed['path'] === $existing_details->path ) {
+			update_option( 'siteurl', $new_url );
+		}
+
+		restore_current_blog();
+		wp_redirect(
+			add_query_arg(
+				array(
+					'update' => 'updated',
+					'id'     => $id,
+				),
+				'site-info.php'
+			)
+		);
+		exit;
 	}
-
-	$old_site_url    = trailingslashit( esc_url( get_option( 'home' ) ) );
-	$old_site_parsed = parse_url( $old_site_url );
-	$old_site_host   = $old_site_parsed['host'] . ( isset( $old_site_parsed['port'] ) ? ':' . $old_site_parsed['port'] : '' );
-
-	restore_current_blog();
-	wp_redirect(
-		add_query_arg(
-			array(
-				'update' => 'updated',
-				'id'     => $id,
-			),
-			'site-info.php'
-		)
-	);
-	exit;
 }
 
 if ( isset( $_GET['update'] ) ) {
@@ -136,6 +157,16 @@ network_edit_site_nav(
 	)
 );
 
+if ( $site_url_error ) {
+	wp_admin_notice(
+		$site_url_error,
+		array(
+			'type'        => 'error',
+			'dismissible' => true,
+		)
+	);
+}
+
 if ( ! empty( $messages ) ) {
 	$notice_args = array(
 		'type'        => 'success',
@@ -161,23 +192,17 @@ if ( ! empty( $messages ) ) {
 			<td><?php echo esc_url( $parsed_scheme . '://' . $details->domain . $details->path ); ?></td>
 		</tr>
 			<?php
-			// For any other site, the scheme, domain, and path can all be changed.
+			// For any other site, the domain and path can be changed.
 		else :
 			?>
 		<tr class="form-field form-required">
 			<th scope="row"><label for="url"><?php _e( 'Site Address (URL)' ); ?></label></th>
-			<td><input name="blog[url]" type="text" id="url" value="<?php echo $parsed_scheme . '://' . esc_attr( $details->domain ) . esc_attr( $details->path ); ?>" /></td>
+			<td>
+				<span class="no-break">https://<input name="blog[url]" type="text" class="regular-text ltr" id="url" value="<?php echo esc_attr( $submitted_site_url ?: $details->domain . $details->path ); ?>" aria-describedby="site-address-description" autocapitalize="none" autocorrect="off" required /></span>
+				<p id="site-address-description" class="description"><?php esc_html_e( 'Before changing the site address, configure DNS and the web server to direct this domain to this CalmPress installation, and install a valid HTTPS certificate for it.' ); ?></p>
+			</td>
 		</tr>
 		<?php endif; ?>
-
-		<tr class="form-field">
-			<th scope="row"><label for="blog_registered"><?php _ex( 'Registered', 'site' ); ?></label></th>
-			<td><input name="blog[registered]" type="text" id="blog_registered" value="<?php echo esc_attr( $details->registered ); ?>" /></td>
-		</tr>
-		<tr class="form-field">
-			<th scope="row"><label for="blog_last_updated"><?php _e( 'Last Updated' ); ?></label></th>
-			<td><input name="blog[last_updated]" type="text" id="blog_last_updated" value="<?php echo esc_attr( $details->last_updated ); ?>" /></td>
-		</tr>
 	</table>
 
 	<?php
